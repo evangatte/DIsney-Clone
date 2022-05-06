aa46a73da20e57864c57fa326cc0b9936a83234a
Evan Gatte <evangatte2255@gmail.com>
2022-05-05 22:16:54 -0500

    create new product and product list route

--- routes/admin.js
+++ routes/admin.js
@@ -1,30 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const Product = require('../src/db').ProductModel;
 
-router.get('/show', (req, res) => {
-    res.render('admin/home', {
-        pageTitle: 'Admin',
-    });
+router.get('/', (req, res) => {
+    res.render('admin/home');
 });
 
 router.get('/products', async (req, res) => {
-    const { ProductModel } = require('../src/db');
-    const products = await ProductModel.find({});
+    const products = await Product.find({});
 
-    res.render('admin/products', {
+    res.render('admin/productList', {
         pageTitle: 'Admin - Products',
         products,
     });
 });
 
-router.post('newProduct', async (req, res) => {
-    // ProductModel.create({
-    //     ID: '1',
-    //     name: 'First Product'
-    // });
-    res.json({
-        params: req.params,
-    });
+router.get('/products/new', (req, res) => {
+    res.render('admin/productForm');
+});
+
+router.post('/newProduct', async (req, res) => {
+    res.render('admin/productForm');
 });
 
 module.exports = router;
